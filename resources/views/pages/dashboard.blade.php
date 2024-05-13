66c955eb5ab9b8b6e9f78bf47657d5907a3acdcd
milalestari <milamilalestari8@gmail.com>
2024-05-13 13:46:47 +0700

    buat tampilan sidebar untuk admin

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,16 +1,10 @@
 @extends('layouts.admin')
-
+@section('title', 'Admin F-OurTalk')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">  
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
-
-                    <div class="card-body">
-                        Selamat datang di dashboard!
-                    </div>
-                </div>
+    <div class="font-[Poppins] h-screen bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] flex justify-left items-start flex-col">
+        <div class="p-20">
+            <div class="text-left ml-60 mt-3">
+                <h1 class="text-3xl font-semibold">Dashboard</h1>
             </div>
         </div>
     </div>
